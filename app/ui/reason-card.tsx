44cf8bfc4ed4e1d9a1cdbc3cc824f46fbb9ea7ab
harvey <harvepq@gmail.com>
2024-04-15 15:22:52 -0500

    Add reason card component

---
+++ app/ui/reason-card.tsx
@@ -0,0 +1,9 @@
+export default function ReasonCard() {
+  return(
+    <div className="flex flex-col gap-4 items-center min-w-72 max-w-80 text-center">
+      <img className="h-14" src="/icons/lock.svg" alt="" />
+      <h3 className="text-xl w-40 leading-6">Seguridad en tus compras</h3>
+      <p className="text-[#6D6D6D]">Aenean ut nulla at purus facilisis rhoncus a a tellus. Quisque pellentesque consequat ex in congue. Nulla eu tincidunt erat. Curabitur sit amet magna lectus. Etiam quis convallis risus.</p>
+    </div>
+  )
+}
