76634cb96e47665c829137e35a049730ea14e6d7
harvey <harvepq@gmail.com>
2024-04-15 16:39:10 -0500

    Add product description component

---
+++ app/ui/products/product-decription.tsx
@@ -0,0 +1,8 @@
+export default function ProductDescription() {
+  return (
+    <div className="px-5">
+      <h1 className="text-3xl font-semibold mb-4">Machu Picchu, una aventrua inolvidable</h1>
+      <p className="text-[#6D6D6D]">Embarcate en el tour del Valle Encantador, una experiencia que te conectara con la serenidad y la belleza natural. Durante la caminata, seras testigo de una laguna impresionante que alberga mas de 100 especies asi como paisajes espectaculares y monumentos culturales. Descubre el famoso bosque de eucaliptos del Cusco, pasa por lugares emblematicos como Qenqo y el templo de la tranquilidad y la magia del valle encantado mientras te conectas con la esencia misma la naturaleza</p>
+    </div>
+  )
+}
