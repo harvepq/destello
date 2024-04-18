9d62e7921d03aacc85748097d12653c20dc6d8b2
harvey <harvepq@gmail.com>
2024-04-18 16:51:06 -0500

    Add about engagement component

---
+++ app/ui/about/engagement.tsx
@@ -0,0 +1,27 @@
+export default function Engagement () {
+  return (
+    <div className="text-center">
+      <h2 className="text-[32px] mb-6">Nuestro Compromiso</h2>
+      <p className="text-[#7D7D7D] my-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean consectetur in eros eu pharetra. Vivamus efficitur nisl quis tincidunt posuere. Donec lacinia molestie ornare. Proin sollicitudin urna sed vulputate fermentum. Donec at dapibus purus, non ultricies nunc. Phasellus sagittis magna sit amet orci rutrum condimentum. Praesent in velit eget neque euismod accumsan. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+      <ul className="flex flex-col gap-5 mx-8 text-[#7D7D7D]">
+        <li className="flex gap-5 items-center text-left py-2 border-dashed border-y border-black">
+          <img className="w-12 h-12" src="/icons/1-square.svg" alt="" />
+          <p>Ofrecer un servicio al cliente de primera clase, personalizado y adaptable a las necesidades individuales de cada viajero.</p>
+        </li>
+        <li className="flex gap-5 items-center text-right py-2 border-dashed border-y border-black">
+          <p>Garantizar la seguridad y el bienestar de nuestros clientes en cada ascpecto de su viaje, desde la planificacion hasta el regreso a casa</p>
+          <img className="w-12 h-12" src="/icons/2-square.svg" alt="" />
+        </li>
+        <li className="flex gap-5 items-center text-left py-2 border-dashed border-y border-black">
+          <img className="w-12 h-12" src="/icons/3-square.svg" alt="" />
+          <p>Fomentar relaciones solidad con nuestros clientes, basados en la confianza, la transparencia y la satisfaccion del cliente</p>
+        </li>
+        <li className="flex gap-5 items-center text-right py-2 border-dashed border-y border-black">
+          <p>Garantizar la seguridad y el bienestar de nuestros clientes en cada ascpecto de su viaje, desde la planificacion hasta el regreso a casa</p>
+          <img className="w-12 h-12" src="/icons/4-square.svg" alt="" />
+        </li>
+      </ul>
+      <p className="text-[#7D7D7D] my-5">Al adherirnos a nuestros principios y estrategias, nos esforzamos por cumplir muestra mision de brindar experiencias de viaje inolvidables y convertirnos en el referente lider en viajes por los paises andino.</p>
+    </div>
+  )
+}
