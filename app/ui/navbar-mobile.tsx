63bf1aa0a08406376914638d3cfd02e977299075
harvey <harvepq@gmail.com>
2024-04-20 16:39:54 -0500

    Include menu icon into button tag, add show menu state and add Menu Mobile component

--- app/ui/navbar-mobile.tsx
+++ app/ui/navbar-mobile.tsx
@@ -1,3 +1,14 @@
+'use client'
+
+// React Imports
+import { useState } from "react"
+
+// Next Imports
+import Link from "next/link"
+
+import MenuMobile from "@/app/ui/menu-mobile";
+
+
 // Icons Name | svg file name
 const icons = [
   {name: 'facebook-icon', url: '#'},
@@ -7,20 +18,28 @@ const icons = [
 ]
 
 export default function NavBarMobile () {
+  // Movile Menu State
+  const [showMenu, setShowMenu] = useState(false)
+
   return (
-    <header>
+    <header className="relative">
       <nav className='flex justify-between items-center h-auto bg-[#052659] px-5 py-4 text-white'>
         <div className="flex justify-between items-center gap-4">
-          <img 
-            className="w-8 h-8" 
-            src="/icons/mobile-menu.svg" 
-            alt="mobile menu icon" 
-          />
+          <button
+            onClick={() => setShowMenu(!showMenu)}
+          >
+            <img
+              className="w-8 h-8"
+              src="/icons/mobile-menu.svg"
+              alt="mobile menu icon"
+            />
+          </button>
           <p className="text-xl font-extrabold leading-6">
             Destello<br />
             &emsp;Andino
           </p>
         </div>
+        <MenuMobile showMenu={showMenu} setShowMenu={setShowMenu}/>
         <ul className="flex gap-4">
           {icons.map(icon => (
             <li key={icon.name}>
