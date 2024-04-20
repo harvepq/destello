42ef1b8a37fb6fe76b3ebb96411abe8552582361
harvey <harvepq@gmail.com>
2024-04-20 12:42:11 -0500

    Change a tag with Link tag and create array links

--- app/ui/menu-mobile.tsx
+++ app/ui/menu-mobile.tsx
@@ -1,31 +1,36 @@
+'use client'
+
+// Import Nextjs Components
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+
+
+// Links
+const links = [
+  {name: 'Inicio', href: '/'},
+  {name: 'Tours', href: '/tours'},
+  {name: 'Blog', href: '/blog'},
+  {name: 'Nosotros', href: '/about'}
+]
+
 export default function MenuMobile () {
+  const pathname = usePathname()
+
   return (
     <div className="bg-black/50">
       <div className="w-4/6 h-svh bg-white p-6 text-center flex flex-col justify-between">
         <div className="">
           <h2 className="text-2xl mb-8">Destello <br /> Andino</h2>
-          <ul className="flex flex-col gap-6">
-            <li>
-              <a href="">
-                Inicio
-              </a>
-            </li>
-            <li>
-              <a href="">
-                Tours
-              </a>
-            </li>
-            <li>
-              <a href="">
-                Blog
-              </a>
-            </li>
-            <li>
-              <a href="">
-                Nosotros
-              </a>
-            </li>
-          </ul>
+          {links.map((link) => (
+            <Link
+              key={link.name}
+              href={link.href}
+              className={`flex h-[40px] grow items-center justify-center rounded-md bg-gray-50 ${pathname === link.href ? 'bg-sky-100 text-blue-600' : ''}`}
+            >
+              <p>{link.name}</p>
+            </Link>
+          ))}
         </div>
         <div className="mb-8">
             <p>Viaja con los mejores del Peru</p>
