d2213f0b90be40c83943aa0203c4232aae31f69b
harvey <harvepq@gmail.com>
2024-04-21 17:52:23 -0500

    Add recive props into reason card

--- app/ui/reason-card.tsx
+++ app/ui/reason-card.tsx
@@ -1,9 +1,9 @@
-export default function ReasonCard() {
+export default function ReasonCard({title, description, icon} : any) {
   return(
     <div className="flex flex-col gap-4 items-center min-w-72 max-w-80 text-center">
-      <img className="h-14" src="/icons/lock.svg" alt="" />
-      <h3 className="text-xl w-40 leading-6">Seguridad en tus compras</h3>
-      <p className="text-[#6D6D6D]">Aenean ut nulla at purus facilisis rhoncus a a tellus. Quisque pellentesque consequat ex in congue. Nulla eu tincidunt erat. Curabitur sit amet magna lectus. Etiam quis convallis risus.</p>
+      <img className="h-14" src={icon} alt="" />
+      <h3 className="text-xl w-40 leading-6">{title}</h3>
+      <p className="text-[#6D6D6D]">{description}</p>
     </div>
   )
 }
