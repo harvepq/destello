abd7e86e8360ee4fce13cb22a5988be40846268b
harvey <harvepq@gmail.com>
2024-04-21 17:41:01 -0500

    Add recive props into news cards

--- app/ui/news-card.tsx
+++ app/ui/news-card.tsx
@@ -1,25 +1,16 @@
 import Image from "next/image"
 
-const news = {
-  id: 'news2',
-  url: '#', 
-  slogan: 'Viaja, Suena y Vive', 
-  location: 'Madre de Dios', 
-  imageName: 'news2', 
-  shortDescription: '??'
-}
-
-export default function NewsCard () {
+export default function NewsCard ({name, alt, image, location} : any) {
   return (
     <div className="flex flex-col items-center relative min-w-72 max-w-96 rounded-xl overflow-hidden">
       <picture>
-        <img className="aspect-[2/1.1] object-cover brightness-90" src={`/icons/${news.imageName}.jpg`}alt={news.shortDescription} />
+        <img className="aspect-[2/1.1] object-cover brightness-90" src={image} alt={alt} />
       </picture>
       <div className="text-white absolute bottom-2">
-        <p className="text-xl">{news.slogan}</p>
+        <p className="text-xl">{name}</p>
         <div className="flex justify-center items-center gap-2">
           <img src="/icons/geo-alt.svg" alt="" />
-          <span className="text-sm">{news.location}</span>
+          <span className="text-sm">{location}</span>
         </div>
       </div>
     </div>
