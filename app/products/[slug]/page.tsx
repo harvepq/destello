30dd31e0f7a32c8287b64844b02b0d8d9e0eb82d
harvey <harvepq@gmail.com>
2024-04-21 22:52:05 -0500

    Add fetch product into product page

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -1,13 +1,21 @@
 import ProductDescription from "@/app/ui/products/product-decription";
 import ProductImage from "@/app/ui/products/product-image";
+import { fetchProductBySlug } from "@/app/lib/data";
 
 export default function Page({ params }: { params: { slug: string } }) {
   const slug = params.slug
+  const product = fetchProductBySlug(slug)
   return (
     <main>
-      <ProductImage />
-      <p>{slug}</p>
-      <ProductDescription />
+      <ProductImage images={product.images}/>
+      <ProductDescription
+        title={product.title}
+        description={product.description}
+        prices={product.prices}
+        people= {product.people}
+        items={product.items}
+        itinerary={product.itinerary}
+      />
     </main>
   )
 }
