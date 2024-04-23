b471260a2c8c4b3c11ffc04e92195ef0dcf07146
harvey <harvepq@gmail.com>
2024-04-23 00:40:46 -0500

    Modify max width, and aspect ratio and text base

--- app/ui/news-card.tsx
+++ app/ui/news-card.tsx
@@ -2,15 +2,15 @@ import Image from "next/image"
 
 export default function NewsCard ({name, alt, image, location} : any) {
   return (
-    <div className="flex flex-col items-center relative min-w-72 max-w-96 rounded-xl overflow-hidden">
+    <div className="flex flex-col items-center relative min-w-72 max-w-md rounded-xl overflow-hidden">
       <picture>
-        <img className="aspect-[2/1.1] object-cover brightness-90" src={image} alt={alt} />
+        <img className="aspect-[2/1.3] object-cover brightness-90" src={image} alt={alt} />
       </picture>
-      <div className="text-white absolute bottom-2">
+      <div className="text-white absolute bottom-4">
         <p className="text-xl">{name}</p>
         <div className="flex justify-center items-center gap-2">
           <img src="/icons/geo-alt.svg" alt="" />
-          <span className="text-sm">{location}</span>
+          <span className="text-base">{location}</span>
         </div>
       </div>
     </div>
