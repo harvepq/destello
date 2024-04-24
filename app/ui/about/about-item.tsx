4eac5c8ca20e589fbc3ebfb813d45e9a882f17b1
harvey <harvepq@gmail.com>
2024-04-23 23:50:04 -0500

    Change some styles

--- app/ui/about/about-item.tsx
+++ app/ui/about/about-item.tsx
@@ -1,9 +1,9 @@
 export default function AboutItem ({item, slogan}: any) {
   return (
-    <div className="text-center">
+    <div className="max-w-md text-center">
       <h2 className="text-[32px] mb-6">{item.title}</h2>
-      <p className="text-[#7D7D7D] my-5">{item.description}</p>
-      <div className="min-w-72 max-w-96 relative flex justify-center m-auto">
+      <p className="text-[#7D7D7D] mb-5">{item.description}</p>
+      <div className="min-w-72 max-w-md relative flex justify-center m-auto">
         <picture>
           <img className="w-full aspect-[9/5] object-cover rounded-xl" src="/icons/news1.jpg" alt="" />
         </picture>
