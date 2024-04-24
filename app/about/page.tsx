0c6d606257d156f0728d879104ed941013f9bb3c
harvey <harvepq@gmail.com>
2024-04-23 23:45:58 -0500

    Add mission and vision item prop into

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,22 +9,22 @@ export default function Page() {
   return (
     <main>
       {/* Hero Image */}
-      <div className="relative w-full flex justify-center">
+      <div className="relative w-full flex flex-col justify-center items-center">
         <picture>
           <img className="w-full aspect-[3/2] object-cover brightness-[0.8]" src="/products/product1.jpeg" alt="" />
         </picture>
-        <div className="absolute bottom-12 text-center">
-          <h2 className="text-white text-3xl">Nosotros</h2>
+        <div className="absolute text-center">
+          <h2 className="text-white text-4xl">Nosotros</h2>
         </div>
       </div>
 
       {/* About Section */}
-      <section className="flex flex-col items-center gap-12 my-10 px-6">
+      <section className="flex flex-col items-center gap-8 my-4 px-6">
         <AboutItem item={about.we_are} slogan="Somos los mejores"/>
         <AboutItem item={about.we_do} slogan="Hacemos que sea lo mejor"/>
         <Engagement item={about.engagement}/>
-        {/* <AboutItem /> */}
-        {/* <AboutItem /> */}
+        <AboutItem item={about.mission} slogan="Ser lo mejores agentes"/>
+        <AboutItem item={about.vision} slogan="Llegar la empresa mas grande del mundo"/>
       </section>
     </main>
   )
