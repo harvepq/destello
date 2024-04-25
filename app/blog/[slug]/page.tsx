5a1bfd585e85eeb44f271975917adadd8346a037
harvey <harvepq@gmail.com>
2024-04-25 15:15:15 -0500

    Add list render of posts into post page

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -4,12 +4,12 @@
 import { useRouter } from "next/navigation";
 
 // Data Imports
-import { fetchProductBySlug } from "@/app/lib/data";
+import { fetchPostBySlug } from "@/app/lib/data";
 
 
 export default function Page({ params }: { params: { slug: string } }) {
   const slug = params.slug
-  const product = fetchProductBySlug(slug)
+  const post = fetchPostBySlug(slug)
 
   const route = useRouter()
 
@@ -36,48 +36,27 @@ export default function Page({ params }: { params: { slug: string } }) {
       </div>
       <div className="w-full">
         <picture>
-          <img className="w-full aspect-[3/2] object-cover" src="/products/product2.jpg" alt="" />
+          <img className="w-full aspect-[3/2] object-cover" src={post.images[0]}alt="" />
         </picture>
       </div>
       <div className="my-4 px-8">
         <div className="max-w-md mx-auto">
-          <h2 className="text-4xl mb-6 mt-2 uppercase">Convierte Tu Marca en un Simbolo de la Identidad de tu cliente</h2>
+          <h2 className="text-4xl mb-6 mt-2 uppercase">{post.name}</h2>
           <div className="flex flex-col gap-4">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean consectetur in eros eu pharetra. Vivamus efficitur nisl quis tincidunt posuere. Donec lacinia molestie ornare. Proin sollicitudin urna sed vulputate fermentum. Donec at dapibus purus, non ultricies nunc. Phasellus sagittis magna sit amet orci rutrum condimentum. Praesent in velit eget neque euismod accumsan. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean consectetur in eros eu pharetra. Vivamus efficitur nisl quis tincidunt posuere. Donec lacinia molestie ornare. Proin sollicitudin urna sed vulputate fermentum. Donec at dapibus purus, non ultricies nunc. Phasellus sagittis magna sit amet orci rutrum condimentum. Praesent in velit eget neque euismod accumsan. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean consectetur in eros eu pharetra. Vivamus efficitur nisl quis tincidunt posuere. Donec lacinia molestie ornare. Proin sollicitudin urna sed vulputate fermentum. Donec at dapibus purus, non ultricies nunc. Phasellus sagittis magna sit amet orci rutrum condimentum. Praesent in velit eget neque euismod accumsan. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean consectetur in eros eu pharetra. Vivamus efficitur nisl quis tincidunt posuere. Donec lacinia molestie ornare. Proin sollicitudin urna sed vulputate fermentum. Donec at dapibus purus, non ultricies nunc. Phasellus sagittis magna sit amet orci rutrum condimentum. Praesent in velit eget neque euismod accumsan. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+            <p>{post.description}</p>
           </div>
         </div>
         <div className="mt-8 flex flex-col items-center gap-4">
-          <div className="min-w-72 max-w-md relative flex m-auto">
-            <picture>
-              <img className="w-full aspect-[8/5] object-cover rounded-xl" src="/icons/news1.jpg" alt="" />
-            </picture>
-            <div className="absolute -top-2 left-2 bg-black/[0.8] text-white rounded-xl px-8 leading-[40px]">Choquequirao - cusco asdf</div>
-          </div>
-          <div className="min-w-72 max-w-md relative flex m-auto">
-            <picture>
-              <img className="w-full aspect-[8/5] object-cover rounded-xl" src="/icons/news1.jpg" alt="" />
-            </picture>
-            <div className="absolute -top-2 left-2 bg-black/[0.8] text-white rounded-xl px-8 leading-[40px]">Choquequirao</div>
-          </div>
-          <div className="min-w-72 max-w-md relative flex m-auto">
-            <picture>
-              <img className="w-full aspect-[8/5] object-cover rounded-xl" src="/icons/news1.jpg" alt="" />
-            </picture>
-            <div className="absolute -top-2 left-2 bg-black/[0.8] text-white rounded-xl px-8 leading-[40px]">Machu Picchu</div>
-          </div>
-          <div className="min-w-72 max-w-md relative flex m-auto">
-            <picture>
-              <img className="w-full aspect-[8/5] object-cover rounded-xl" src="/icons/news1.jpg" alt="" />
-            </picture>
-            <div className="absolute -top-2 left-2 bg-black/[0.8] text-white rounded-xl px-8 leading-[40px]">Cerro de 7 colores</div>
-          </div>
+          {post.images.map(image => (
+            <div className="min-w-72 max-w-md relative flex m-auto" key={image}>
+              <picture>
+                <img className="w-full aspect-[8/5] object-cover rounded-xl" src={image} alt="" />
+              </picture>
+              <div className="absolute -top-2 left-2 bg-black/[0.8] text-white rounded-xl px-8 leading-[40px]">Choquequirao - cusco asdf</div>
+            </div>
+          ))}
         </div>
-
       </div>
-
     </main>
   )
 }
