479ac6ca54aaf5e909697f7d99aa233bb89ed0e5
harvey <harvepq@gmail.com>
2024-04-26 19:22:27 -0500

    Add media query inside about items and engagement component

--- app/ui/about/about-item.tsx
+++ app/ui/about/about-item.tsx
@@ -1,13 +1,17 @@
-export default function AboutItem ({item, slogan}: any) {
+export default function AboutItem ({item, slogan, className}: any) {
   return (
-    <div className="max-w-md text-center">
-      <h2 className="text-[32px] mb-6">{item.title}</h2>
-      <p className="text-[#7D7D7D] mb-5">{item.description}</p>
-      <div className="min-w-72 max-w-md relative flex justify-center m-auto">
-        <picture>
-          <img className="w-full aspect-[9/5] object-cover rounded-xl" src="/icons/news1.jpg" alt="" />
-        </picture>
-        <div className="absolute -bottom-2 bg-black/[0.8] text-white rounded-xl w-[90%] leading-[40px]">{slogan}</div>
+    <div className="max-w-md text-center md:max-w-fit 2xl:w-[1500px]">
+      <div className={`md:flex md:text-left md:gap-10 xl:gap-24 items-center ${className}`}>
+        <div>
+          <h2 className="text-[32px] mb-6 text-center">{item.title}</h2>
+          <p className="text-[#7D7D7D] mb-5">{item.description}</p>
+        </div>
+        <div className="min-w-72 max-w-md relative flex justify-center m-auto md:min-w-[46%]">
+          <picture className="w-full">
+            <img className="w-full aspect-[9/5] object-cover rounded-xl" src="/icons/news1.jpg" alt="" />
+          </picture>
+          <div className="absolute -bottom-2 bg-black/[0.8] text-white rounded-xl w-[90%] leading-[40px] text-center">{slogan}</div>
+        </div>
       </div>
     </div>
   )
--- app/ui/about/engagement.tsx
+++ app/ui/about/engagement.tsx
@@ -1,6 +1,6 @@
 export default function Engagement ({item}:any) {
   return (
-    <div className="text-center">
+    <div className="text-center md:max-w-[800px]">
       <h2 className="text-[32px] mb-6">{item.title}</h2>
       <p className="text-[#7D7D7D] my-5">{item.description}</p>
       <ul className="flex flex-col gap-5 mx-8 text-[#7D7D7D]">
@@ -8,7 +8,7 @@ export default function Engagement ({item}:any) {
           <img className="w-12 h-12" src="/icons/1-square.svg" alt="" />
           <p>{item.first}</p>
         </li>
-        <li className="flex gap-5 items-center text-right py-2 border-dashed border-y border-black">
+        <li className="flex gap-5 items-center text-left py-2 border-dashed border-y border-black">
           <p>{item.second}</p>
           <img className="w-12 h-12" src="/icons/2-square.svg" alt="" />
         </li>
@@ -16,7 +16,7 @@ export default function Engagement ({item}:any) {
           <img className="w-12 h-12" src="/icons/3-square.svg" alt="" />
           <p>{item.third}</p>
         </li>
-        <li className="flex gap-5 items-center text-right py-2 border-dashed border-y border-black">
+        <li className="flex gap-5 items-center text-left py-2 border-dashed border-y border-black">
           <p>{item.fourth}</p>
           <img className="w-12 h-12" src="/icons/4-square.svg" alt="" />
         </li>
