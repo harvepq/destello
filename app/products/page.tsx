d9d5dc6fc56b2842d2a3adb639d0b3664f8c2cee
harvey <harvepq@gmail.com>
2024-05-14 17:03:08 -0500

    Add hero image with relative path to product page

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
       {/* Hero Image */}
       <div className="relative w-full flex flex-col justify-center items-center">
         <picture className="w-full">
-          <img className="w-full aspect-[3/2] object-cover brightness-[0.8] sm:h-[480px] md:h-[600px] lg:h-[720px]" src="/products/product1.jpeg" alt="" />
+          <img className="w-full aspect-[3/2] object-cover brightness-[0.8] sm:h-[480px] md:h-[600px] lg:h-[720px]" src="/hero/hero-product.jpg" alt="" />
         </picture>
         <div className="absolute text-center">
           <h2 className="text-white text-4xl">Productos</h2>
@@ -20,7 +20,7 @@ export default function Page() {
           <h3 className="uppercase text-[#D2984E]">nuesstros productos</h3>
         </div>
         <div className="px-7 lg:px-20">
-          <h2 className="uppercase mb-6 text-3xl">Convierte tu viaje en un simbolo de identidad de tu cliente</h2>
+          <h2 className="uppercase mb-6 text-3xl lg:my-8">Convierte tu viaje en un simbolo de identidad</h2>
           <div className="flex flex-col gap-6 items-center sm:grid sm:grid-cols-2 sm:place-items-center sm:gap-x-6 sm:gap-y-10 xl:grid-cols-3 2xl:grid-cols-4">
             <ProductList />
           </div>
