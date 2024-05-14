284e74a9a9b79c49119e0956b0fdbd5dc04b463d
harvey <harvepq@gmail.com>
2024-05-14 16:13:29 -0500

    Add hover:scale to product card and change hover color view details button

--- app/ui/product-card.tsx
+++ app/ui/product-card.tsx
@@ -1,6 +1,6 @@
 export default function ProductCard({name, image, items, prices, people, description}: any) {
   return (
-    <div className="min-w-72 max-w-md shadow-[0px_0px_8px_2px_rgba(0,0,0,0.25)] rounded-xl overflow-hidden cursor-pointer">
+    <div className="min-w-72 max-w-md shadow-[0px_0px_8px_2px_rgba(0,0,0,0.25)] rounded-xl overflow-hidden cursor-pointer sm:hover:scale-105">
       <picture>
         <img className="aspect-[2/1.5] object-cover" src={`products/${image}`} alt={name} />
       </picture>
@@ -21,7 +21,7 @@ export default function ProductCard({name, image, items, prices, people, descrip
             <p>{items.tour}</p>
           </div>
         </div>
-        <button className="w-full bg-[#052659] text-white text-lg font-bold px-6 h-12 rounded-lg hover:bg-sky-800 my-4">Ver detalle</button>
+        <button className="w-full bg-[#052659] text-white text-lg font-bold px-6 h-12 rounded-lg hover:bg-blue-900 my-4">Ver detalle</button>
         {/* <span className="text-[#7D7D7D] block leading-4" >Desde</span>
         <div className="flex justify-between">
           <div className="flex items-end gap-2">
