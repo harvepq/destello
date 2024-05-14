abfa0faeee22c0d6c9e37dd5ce4d7b3ee4df20e9
harvey <harvepq@gmail.com>
2024-05-14 14:43:44 -0500

    Add relative path into image url

--- app/ui/products/product-image.tsx
+++ app/ui/products/product-image.tsx
@@ -5,7 +5,7 @@ export default function ProductImage({images, setShowImages}: any) {
         onClick={() => {setShowImages(true)}}
       >
         <picture>
-          <img className="w-full aspect-[3/2] object-cover md:aspect-[1]" src={images[0]} alt="" />
+          <img className="w-full aspect-[3/2] object-cover md:aspect-[1]" src={`/products/${images[0]}`} alt="" />
         </picture>
         <div className="absolute flex justify-end items-end inset-0 md:hidden">
           <div className="text-white bg-[#222222]/[0.66] rounded text-sm px-3 py-1 mr-3 mb-4">
@@ -16,22 +16,22 @@ export default function ProductImage({images, setShowImages}: any) {
       <div className="hidden md:grid md:grid-cols-2 md:gap-2">
         <div className="w-full cursor-pointer">
           <picture>
-            <img className="w-full aspect-[1] object-cover" src={images[1]} alt="" />
+            <img className="w-full aspect-[1] object-cover" src={`/products/${images[1]}`} alt="" />
           </picture>
         </div>
         <div className="w-full cursor-pointer rounded-tr-xl overflow-hidden">
           <picture>
-            <img className="w-full aspect-[1] object-cover" src={images[1]} alt="" />
+            <img className="w-full aspect-[1] object-cover" src={`/products/${images[2]}`} alt="" />
           </picture>
         </div>
         <div className="w-full cursor-pointer">
           <picture>
-            <img className="w-full aspect-[1] object-cover" src={images[1]} alt="" />
+            <img className="w-full aspect-[1] object-cover" src={`/products/${images[3]}`} alt="" />
           </picture>
         </div>
         <div className="w-full cursor-pointer rounded-br-xl overflow-hidden">
           <picture>
-            <img className="w-full aspect-[1] object-cover" src={images[1]} alt="" />
+            <img className="w-full aspect-[1] object-cover" src={`/products/${images[4]}`} alt="" />
           </picture>
         </div>
       </div>
