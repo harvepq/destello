eda459f445463452038c53154b0997b68c8cdb98
harvey <harvepq@gmail.com>
2024-05-14 16:56:15 -0500

    Add hero image with relative path, paragraphs, and increase images into slug blog

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -10,12 +10,13 @@ import { fetchPostBySlug } from "@/app/lib/data";
 export default function Page({ params }: { params: { slug: string } }) {
   const slug = params.slug
   const post = fetchPostBySlug(slug)
+  const postImages = post.images.slice(1)
 
   const route = useRouter()
 
   return (
     <main className="relative">
-      <div className="p-4 absolute z-10">
+      <div className="p-4 absolute z-10 md:hidden">
         <button
           className="h-9 w-9 inline-flex justify-center items-center border rounded-full bg-white cursor-pointer border-solid shadow-xl"
           onClick={() => route.back()}
@@ -36,23 +37,25 @@ export default function Page({ params }: { params: { slug: string } }) {
       </div>
       <div className="w-full">
         <picture>
-          <img className="w-full aspect-[3/2] object-cover brightness-[0.8] sm:h-[480px] md:h-[600px] lg:h-[720px]" src={post.images[0]}alt="" />
+          <img className="w-full aspect-[3/2] object-cover brightness-[0.8] sm:h-[480px] md:h-[600px] lg:h-[720px]" src={`/blog/${post.images[0]}`}alt="" />
         </picture>
       </div>
       <div className="my-4 px-8 lg:px-20">
         <div className="max-w-md mx-auto md:max-w-3xl">
-          <h2 className="text-4xl mb-6 mt-2 uppercase">{post.name}</h2>
-          <div className="flex flex-col gap-4">
-            <p>{post.description}</p>
+          <h2 className="text-4xl mb-6 mt-2 uppercase md:mt-10">{post.name}</h2>
+          <div className="flex flex-col gap-4 text-gray-600">
+            {post.description.map(paragraph => (
+              <p key={paragraph}>{paragraph}</p>
+            ))}
           </div>
         </div>
         <div className="mt-8 flex flex-col items-center gap-4">
-          {post.images.map(image => (
-            <div className="min-w-72 max-w-md relative flex m-auto" key={image}>
+          {postImages.map(image => (
+            <div className="min-w-72 max-w-md relative flex m-auto md:max-w-3xl" key={image}>
               <picture>
-                <img className="w-full aspect-[8/5] object-cover rounded-xl" src={image} alt="" />
+                <img className="w-full aspect-[8/5] object-cover rounded-xl" src={`/blog/${image}`} alt="" />
               </picture>
-              <div className="absolute -top-2 left-2 bg-black/[0.8] text-white rounded-xl px-8 leading-[40px]">Choquequirao - cusco asdf</div>
+              {/* <div className="absolute -top-2 left-2 bg-black/[0.8] text-white rounded-xl px-8 leading-[40px]">Choquequirao - cusco asdf</div> */}
             </div>
           ))}
         </div>
