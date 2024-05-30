7202b9f6be9c29d37a4cc519c09f71bb1017c4c9
harvey <harvepq@gmail.com>
2024-05-30 18:51:13 -0500

    Fix whatsappme number and reasons rendering styles

--- app/page.tsx
+++ app/page.tsx
@@ -21,8 +21,8 @@ export default function Home() {
             <span className="block text-right md:hidden">Andino</span>
             <span className="hidden md:block md:text-4xl lg:text-5xl">&quot;La vida no se mide por el número de respiraciones que tenemos, sino por los sitios y momentos que nos quitan la respiración.&quot;</span>
           </h1>
-          <Link 
-            href="https://wa.me/51928505264?text=Estoy%20interesado%20en%20viajar%20con%20Destello%20Andino.%20Necesito%20más%20información"
+          <Link
+            href="https://wa.me/51929589891?text=Estoy%20interesado%20en%20viajar%20con%20Destello%20Andino.%20Necesito%20más%20información"
             target="_blank"
             className="bg-[#052659] text-white text-lg font-bold px-6 py-4 rounded-lg hover:bg-white hover:text-black">Solicitar información</Link>
         </div>
@@ -51,7 +51,7 @@ export default function Home() {
       {/* Reasons Section */}
       <section className="px-6 py-6 bg-[#EFF0F2] lg:px-20 lg:py-12">
         <h2 className="text-2xl text-center mb-6 uppercase font-bold lg:text-4xl lg:mb-8">¿Por Qué Viajar Con <br /> Destello Andino?</h2>
-        <div className="flex flex-col items-center gap-8 sm:flex-row sm:justify-center sm:flex-wrap sm:py-10 sm:gap-12">
+        <div className="flex flex-col items-center sm:items-start gap-8 sm:flex-row sm:justify-center sm:flex-wrap sm:py-10 md:gap-24">
           <ReasonList />
         </div>
       </section>
