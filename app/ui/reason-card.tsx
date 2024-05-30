921d584dbd5692f5e49af6889816137c06c0e20d
harvey <harvepq@gmail.com>
2024-05-30 16:13:05 -0500

    Change reason card to print paragraphs

--- app/ui/reason-card.tsx
+++ app/ui/reason-card.tsx
@@ -1,9 +1,20 @@
-export default function ReasonCard({title, description, icon} : any) {
-  return(
-    <div className="flex flex-col gap-4 items-center min-w-72 max-w-80 text-center">
+
+interface ReasonCardProps {
+  title: string;
+  description: string[];
+  icon: string;
+}
+
+const ReasonCard: React.FC<ReasonCardProps> = ({ title, description, icon }) => {
+  return (
+    <div className="flex flex-col gap-4 items-center justify-start min-w-72 max-w-80 text-center">
       <img className="h-14" src={icon} alt="" />
       <h3 className="text-xl w-40 leading-6">{title}</h3>
-      <p className="text-[#6D6D6D]">{description}</p>
+      {description.map((descrip, i) => (
+        <p key={i} className="text-gray-600">{descrip}</p>
+      ))}
     </div>
-  )
-}
+  );
+};
+
+export default ReasonCard;
