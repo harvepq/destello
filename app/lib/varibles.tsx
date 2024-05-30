54bfb62d2d7179800145ea2d4437cc84557a785d
harvey <harvepq@gmail.com>
2024-05-30 14:50:33 -0500

    Add social media links address

--- app/lib/varibles.tsx
+++ app/lib/varibles.tsx
@@ -1,9 +1,9 @@
 // Social Networks - Icons in svg
 export const socialNetworks = [
-  {name: 'facebook', icon: 'facebook-icon', href: '#'},
-  {name: 'instragram', icon: 'instagram-icon', href: '#'},
-  {name: 'whatsapp', icon: 'whatsapp-icon', href: '#'},
-  {name: 'tiktok', icon: 'tiktok-icon', href: '#'},
+  {name: 'facebook', icon: 'facebook-icon', href: 'https://web.facebook.com/profile.php?id=61553464588443'},
+  {name: 'instragram', icon: 'instagram-icon', href: 'https://www.instagram.com/destelloandino_travel?utm_source=qr&igsh=aWVvNGl4YWhrNmY0'},
+  {name: 'whatsapp', icon: 'whatsapp-icon', href: 'https://wa.me/51929589891?text=Solicito%20información'},
+  {name: 'tiktok', icon: 'tiktok-icon', href: 'https://www.tiktok.com/@destelloandinotravel?is_from_webapp=1&sender_device=pc'},
 ]
 
 // Links
